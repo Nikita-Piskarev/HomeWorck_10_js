84d33b39979417486870c4ed369dc8217bece8d8
Nikita Piskarev <npiskarev23@gmail.com>
2022-09-16 17:51:08 +0300

    Fix arrey commit

--- index.js
+++ index.js
@@ -5,15 +5,16 @@
 
 // Функция, которая получает строку и возвращает размер самого большого слова в ней
 
-// const str = "The MDN Web Docs site provides information about Open Web technologies including HTML";
-// const arr = str.split(" ");
-// const returnBigWord = (arr) => {
-//   let word = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     if (word < arr[i].length) {
-//       word = arr[i].length;
-//     }
-//   }
-//   return word;
-// };
+const str =
+  "The MDN Web Docs site provides information about Open Web technologies including HTML";
 
+const returnBigWord = (arr) => {
+  const arrey = arr.split(" ");
+  let word = 0;
+  for (let i = 0; i < arrey.length; i++) {
+    if (word < arrey[i].length) {
+      word = arrey[i].length;
+    }
+  }
+  return word;
+};
