6a6ede3fa77268338e08e1dc8b6f352804aed467
Nikita Piskarev <npiskarev23@gmail.com>
2022-09-16 15:18:18 +0300

    Add function returnNumWord and returnBigWord commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const str = "hello";
+// Функция которая принимает строку и возвращает количество слов в ней.
+// const str ="The MDN Web Docs site provides information about Open Web technologies including HTML";
 
-const returnNumWord = (str) => {
-    str.split();
-    console.log(str);
-};
+// const returnNumWord = (string) => string.split(" ").length;
+
+// Функция, которая получает строку и возвращает размер самого большого слова в ней
+
+// const str = "The MDN Web Docs site provides information about Open Web technologies including HTML";
+// const arr = str.split(" ");
+// const returnBigWord = (arr) => {
+//   let word = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (word < arr[i].length) {
+//       word = arr[i].length;
+//     }
+//   }
+//   return word;
+// };
+
